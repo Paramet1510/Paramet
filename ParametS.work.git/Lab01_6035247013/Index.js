87cdb61b8b5e89bf4b5bd8a6be08f792912bd290
Paramet1510 <paramet.s@psru.ac.th>
2019-12-13 20:44:55 +0700

    initial commitแก

---
+++ ParametS.work.git/Lab01_6035247013/Index.js
@@ -0,0 +1,21 @@
+console.log("Paramet Sukaha");
+console.log("ปารเมศ สุขะหา");
+//alert("Please Refresh")
+let firstName="Paramet"
+let lastName ="Tobtub"
+let age=5;
+let height=168;
+
+function squareNumber(number)
+{   
+    return number*number;
+
+}
+let result=squareNumber(5);
+
+function equation(x,y,z)
+{   
+    return ((x-1)+y)*z;
+
+}
+let resultequation=equation(2,1,6);
